976aaec0bef21318c71773e2f29e7244e0188afe
Ferdxd <74109477+Ferdxd@users.noreply.github.com>
2022-10-02 22:16:53 -0600

    Add files via upload

---
+++ ej3/index.js
@@ -0,0 +1,7 @@
+const logger=require('logplease');
+const log=logger.create('utils');
+
+log.debug('Hola Mundo de Node');
+log.info('¡Información de último momento, Node.js es lo más!! ');
+log.warn('Tirando warnings como campeones');
+log.error('¡Algo no está bien!!!');
